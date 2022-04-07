8892685da8a8ab0268b5f99bb55b9298858a246f
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-04-07 20:27:28 +0100

    ✨ Create class NegativeNumberException

---
+++ src/main/java/org/katasLeanMind/exceptions/NegativeNumberException.java
@@ -0,0 +1,7 @@
+package org.katasLeanMind.exceptions;
+
+public class NegativeNumberException extends Exception{
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
