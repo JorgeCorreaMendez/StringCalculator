21ef1651dbdadb078da998860e3b2b298d955063
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-04-07 10:59:37 +0100

    ✨ Added new method that return zero when get a empty string

--- src/main/java/org/katasLeanMind/StringCalculator.java
+++ src/main/java/org/katasLeanMind/StringCalculator.java
@@ -1,4 +1,12 @@
 package org.katasLeanMind;
 
 public class StringCalculator {
+    public StringCalculator() {}
+
+    public int add(String numbers){
+        if (numbers.equals("")) return 0;
+
+        return 1000;
+    }
+
 }
