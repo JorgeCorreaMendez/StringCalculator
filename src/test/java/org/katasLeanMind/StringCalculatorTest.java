66f55edee024192fa2ed553aed1134c5952c17b7
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-04-07 20:50:34 +0100

    🔨 Refactor StringCalculatorTest Class

--- src/test/java/org/katasLeanMind/StringCalculatorTest.java
+++ src/test/java/org/katasLeanMind/StringCalculatorTest.java
@@ -3,12 +3,9 @@ package org.katasLeanMind;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.katasLeanMind.exceptions.InvalidFormatException;
-import org.katasLeanMind.exceptions.negativeNumberException;
-
-import java.net.PortUnreachableException;
+import org.katasLeanMind.exceptions.NegativeNumberException;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -17,43 +14,61 @@ public class StringCalculatorTest {
     StringCalculator newStringCalculator = new StringCalculator();
 
     @Test
-    public void parse_zero_when_not_receive_number() throws InvalidFormatException {
-        assertEquals(0, newStringCalculator.add(""));
+    public void parse_zero_when_not_receive_number() {
+        try {
+            assertEquals(0, newStringCalculator.add(""));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     @Ignore
-    public void parse_zero_when_receive_more_of_two_numbers() throws InvalidFormatException {
-        assertEquals(0, newStringCalculator.add("1,2,3"));
+    public void parse_zero_when_receive_more_of_two_numbers() {
+        try {
+            assertEquals(0, newStringCalculator.add("1,2,3"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     @Ignore
-    public void sum_numbers_when_receive_two_numbers() throws InvalidFormatException {
-        assertEquals(10, newStringCalculator.add("5,5"));
+    public void sum_numbers_when_receive_two_numbers() {
+        try {
+            assertEquals(10, newStringCalculator.add("5,5"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     @Ignore
-    public void sum_numbers_when_receive_two_numbers_with_separator() throws InvalidFormatException {
-        assertEquals(6, newStringCalculator.add("1\n5"));
+    public void sum_numbers_when_receive_two_numbers_with_separator() {
+        try {
+            assertEquals(6, newStringCalculator.add("1\n5"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test(expected = InvalidFormatException.class)
     @Ignore
-    public void throw_exception_when_receive_two_numbers_with_bad_format() throws InvalidFormatException {
+    public void throw_exception_when_receive_two_numbers_with_bad_format() throws InvalidFormatException, NegativeNumberException {
         newStringCalculator.add("1,\n8");
     }
 
     @Test
-    public void sum_number_when_receive_two_numbers_with_specific_separator() throws InvalidFormatException {
-        assertEquals(3, newStringCalculator.add("//;\n1;2"));
+    public void sum_number_when_receive_two_numbers_with_specific_separator() {
+        try {
+            assertEquals(3, newStringCalculator.add("//;\n1;2"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    @Test(expected = negativeNumberException.class)
-    public void throw_exception_when_receive_negative_numbers() throws InvalidFormatException {
+    @Test(expected = NegativeNumberException.class)
+    public void throw_exception_when_receive_negative_numbers() throws InvalidFormatException, NegativeNumberException {
         newStringCalculator.add("//;\n-1;-1");
     }
-
-
 }
