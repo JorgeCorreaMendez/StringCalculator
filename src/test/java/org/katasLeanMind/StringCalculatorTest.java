fafe301ac90ffa0884034767c2c184d155483c54
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-04-07 10:52:49 +0100

    ✅ Added first test for StringCalculator.add

---
+++ src/test/java/org/katasLeanMind/StringCalculatorTest.java
@@ -0,0 +1,24 @@
+package org.katasLeanMind;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class StringCalculatorTest {
+    StringCalculator newStringCalculator = new StringCalculator();
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void parse_zero_when_not_receive_number(){
+        assertEquals(0, newStringCalculator.add(""));
+    }
+}
