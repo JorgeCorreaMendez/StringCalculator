d672f35e2f5bc3e2c4c0e8d2d11ca241308cd904
JorgeCorreaMendez <jorgecorreamen@gmail.com>
2022-04-07 19:34:07 +0100

    🔨 Added use for new Exception and add new separator \n

--- src/main/java/org/katasLeanMind/StringCalculator.java
+++ src/main/java/org/katasLeanMind/StringCalculator.java
@@ -1,13 +1,20 @@
 package org.katasLeanMind;
 
+import org.katasLeanMind.exceptions.InvalidFormatException;
+
+
+
 public class StringCalculator {
     public StringCalculator() {
     }
 
-    public int add(String numberText) {
+    public int add(String numberText) throws InvalidFormatException {
         if (numberText.equals("")) return 0;
 
-        String[] numbersText = numberText.split(",");
+        if (numberText.contains(",\n"))
+            throw new InvalidFormatException("Error, Invalid format, you can't use ,\\n");
+
+        String[] numbersText = numberText.split("[\n,]");
         int numberOfNumbers = numbersText.length;
 
         if (numberOfNumbers > 2) return 0;
@@ -18,7 +25,7 @@ public class StringCalculator {
     private int sum(String[] numbers) {
         int total = 0;
 
-        for (String number: numbers) {
+        for (String number : numbers) {
             total += Integer.parseInt(number);
         }
 
--- src/test/java/org/katasLeanMind/StringCalculatorTest.java
+++ src/test/java/org/katasLeanMind/StringCalculatorTest.java
@@ -1,6 +1,5 @@
 package org.katasLeanMind;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.katasLeanMind.exceptions.InvalidFormatException;
 
@@ -14,35 +13,32 @@ public class StringCalculatorTest {
     StringCalculator newStringCalculator = new StringCalculator();
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 
     @Test
-    public void parse_zero_when_not_receive_number(){
+    public void parse_zero_when_not_receive_number() throws InvalidFormatException {
         assertEquals(0, newStringCalculator.add(""));
     }
 
     @Test
-    public void parse_zero_when_receive_more_of_two_numbers(){
+    public void parse_zero_when_receive_more_of_two_numbers() throws InvalidFormatException {
         assertEquals(0, newStringCalculator.add("1,2,3"));
     }
 
     @Test
-    public void sum_numbers_when_receive_two_numbers(){
+    public void sum_numbers_when_receive_two_numbers() throws InvalidFormatException {
         assertEquals(10, newStringCalculator.add("5,5"));
     }
 
     @Test
-    public void sum_numbers_when_receive_two_numbers_with_separator(){
-        assertEquals(2, newStringCalculator.add("1/n5"));
+    public void sum_numbers_when_receive_two_numbers_with_separator() throws InvalidFormatException {
+        assertEquals(6, newStringCalculator.add("1\n5"));
     }
 
-
-    @Ignore
     @Test(expected = InvalidFormatException.class)
-    public void throw_exception_when_receive_two_numbers_with_bad_format(){
-        assertEquals(10, newStringCalculator.add("1,/n8"));
+    public void throw_exception_when_receive_two_numbers_with_bad_format() throws InvalidFormatException {
+        newStringCalculator.add("1,\n8");
     }
 }
